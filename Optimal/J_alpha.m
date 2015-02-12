ce25ccb1495b5baff28cf33dd7330db702dd63ed
ginatorres <gina.torres@upc.edu>
2015-02-12 18:39:01 +0100

    J_alpha y vps
    Relación del coste y el parámetro alpha de Q, además de la relación
    entre los vps encontrados con el coste.

--- Optimal/J_alpha.m
+++ Optimal/J_alpha.m
@@ -10,7 +10,7 @@ B=[0;1];
 C=[1 0];
 h=1;
 n=size(A,2);
-Q=[10 0;0 1];
+Q=[10 0;0 10];
 R=1;
 %N=[1; 1]
 [Kd,Sd,Ed]=lqrdg(A,B,Q,1,1);
@@ -29,54 +29,43 @@ m=0;
 for alpha=0:0.1:4  
 m=m+1;
 qq=Q+[0 -alpha*i;alpha*i 0];
-Nn=N+[alpha*i;-alpha*i]
-%qq=Q+[-alpha*i 0;0 alpha*i ]; % No es simètrica
+%Nn=N+[alpha*i;-alpha*i]
 Qx(:,:,m)=qq;
-%[k,s,e,Qd,Nd,Rd]=lqrdg(A,B,qq,1,h);
 [K,S,E,Qd,Nd,Rd]=lqrdg(A,B,qq,R,N,h)
 ks(1,:,m)=K;
+Qds(:,:,m)=Qd
 Ss(:,:,m)=S
 Jx=0.5*(xo'*S*xo);
 Jj(1,m)=Jx;
+Es(:,:,m)=E;
+%[phi,gamma]=c2d(A,B,h);
+%phcl=phi-gamma*K;
+% [ms,es]=eig(phcl);
+% ess(:,:,m)=es
+% mss(:,:,m)=ms
 end
 %Gráfica
 alpha=0:.1:4;
 plot(alpha,Jj,'r')
+
+%% Relación del Coste con la Parte real polos y compleja de los polos obtenidos con los distinos Ks, a través del parámetro complejo alpha de Q
+pr=real(Es)%Parte real
+pc=imag(Es)%Parte compleja
+pa=abs(Es)%Modulo
+
+pr=reshape(pr,2,m)
+pc=reshape(pc,2,m)
+pa=reshape(pa,2,m)
+figure, 
+plot(alpha,pr(1,:),'r+',alpha,pr(2,:),'g+')
+figure,
+plot(alpha,pc(1,:),'ro',alpha,pc(2,:),'go')
 xlabel('alpha, coeficiente complejo')
-ylabel('J, coste')
-[phi,gamma]=c2d(A,B,h)
-
-%Jx =(xo'*Qd*xo + (-inv(Rd)*gamma'*S*xo)'*Rd*(-inv(Rd)*gamma'*S*xo) + 2*xo'*Nd*(-inv(Rd)*gamma'*S*xo))
-%No da lo mismo que J=xo'*S*xo, pues debería hacer la sumatoria de 0 hasta N
-
-%Relación parte compleja de Q y retraso tau
-for m=1:10
-pcl=phi-gamma*ks(1,:,m)
-pcls(:,:,m)=pcl
-Es(:,:,m)=eig(pcls(:,:,m))%Polos complejos no conjugados entonces d=2, debo estudiar el gacker para periodos màs largos que el periodo de muestreo
-end
-
-%% Usando la solución iterativa de la ecuación de ricatti en discreto
-%Manualmente con el tèrmino cruzado N Página 311 Astrom
-%Coste con matrices de pesado con parámetros complejos
-Qx=zeros(n,n,10);
-Jj=zeros(1,10);
-ks=zeros(1,n,10)
-m=0;
-for alpha=0:0.1:4  
-m=m+1;
-qq=Qd+[0 -alpha*i;alpha*i 0];
-%qq=Q+[-alpha*i 0;0 alpha*i ]; % No es simètrica
-Qx(:,:,m)=qq;
-k=10
-while k>=1
-Km(k,:)=vpa(inv(Rd+gamma'*S*gamma)*(gamma'*S*phi+Nd'));
-S=(phi-gamma*Km(k,:))'*S*(phi-gamma*Km(k,:))+Qd-Nd'*Km(k,:)'-Nd*Km(k,:)+Km(k,:)'*Rd*Km(k,:);
-k=k-1;
-end
-ks(1,:,m)=Km;
-Jx=0.5*(xo'*S*xo);
-Jj(1,m)=Jx;
-end
-
+ylabel('Partes reales y complejas valores propios')
+figure, 
+plot(alpha,pa(1,:),'r+',alpha,pa(2,:),'g+')
+figure
+plot(pa(1,:),Jj)
+xlabel('Módulo Valores propios')
+ylabel('Coste')
 
